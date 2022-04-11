699a1e3657573e7244ebd54b0fae4605e31ef766
Jeriel Ng <ngjeriel59@gmail.com>
2022-04-11 20:41:24 +0800

    manageaccount  done

--- ManageAccount.aspx.cs
+++ ManageAccount.aspx.cs
@@ -5,15 +5,179 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace WebApplication
 {
     public partial class ManageAccount : System.Web.UI.Page
     {
+        string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblId.Text = Membership.GetUser().ProviderUserKey.ToString();
             user_name.Text=Membership.GetUser().UserName;
             user_email.Text =Membership.GetUser().Email;
+
+            if (!Page.IsPostBack)
+            {
+                bool found = false;
+
+                string id = Membership.GetUser().ProviderUserKey.ToString();
+
+                string sql = "SELECT * FROM profile WHERE userId =@Id";
+
+                SqlConnection con = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    found = true;
+                    user_realname.Text = (dr["name"]).ToString();
+                    user_address.Text = (dr["address"]).ToString();
+                    newR.Text = "1";
+
+                }
+                else
+                {
+                    newR.Text = "0";
+                }
+                dr.Close();
+                con.Close();
+                                
+            }
+           
+        }
+
+ 
+
+        protected void editRealname_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string userId = lblId.Text;
+                string newRegistration = newR.Text;
+                string name = user_realname.Text;
+                string address = "";
+                if (newRegistration == "0")
+                {
+                    string sql = "INSERT INTO profile(userId,address,name) VALUES (@UserId,@Address,@Name)";
+
+                    SqlConnection con = new SqlConnection(cs);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+
+                    cmd.Parameters.AddWithValue("@UserId", userId);                    
+                    cmd.Parameters.AddWithValue("@Address", address);
+                    cmd.Parameters.AddWithValue("@Name", name);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Your Name has been Registered Sucessfully!')", true);
+
+                    Response.Redirect("ManageAccount.aspx");
+                }
+                else{
+                    string sql = @"UPDATE profile SET name= @Name WHERE userId = @UserId  ";
+                    SqlConnection con = new SqlConnection(cs);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Your Name has changed successfully!')", true);
+
+                    Response.Redirect("ManageAccount.aspx");
+                }
+                
+            }
+        }
+
+        protected void editAddress_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string userId = lblId.Text;
+                string newRegistration = newR.Text;
+                string name = "";
+                string address = user_address.Text;
+                if (newRegistration == "0")
+                {
+                    string sql = "INSERT INTO profile(userId,address,name) VALUES (@UserId,@Address,@Name)";
+
+                    SqlConnection con = new SqlConnection(cs);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+                    cmd.Parameters.AddWithValue("@Address", address);
+                    cmd.Parameters.AddWithValue("@Name", name);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Your Address has been Registered Sucessfully!')", true);
+
+                    Response.Redirect("ManageAccount.aspx");
+                }
+                else
+                {
+                    string sql = @"UPDATE profile SET address= @Address WHERE userId = @UserId  ";
+                    SqlConnection con = new SqlConnection(cs);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+
+                    cmd.Parameters.AddWithValue("@Address", address);
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Your Address has changed successfully!')", true);
+
+                    Response.Redirect("ManageAccount.aspx");
+                }
+
+            }
+        }
+
+        protected void editEmail_Click(object sender, EventArgs e)
+        {
+
+            if (Page.IsValid)
+            {
+
+                ////string id = lblId.Text;
+                //string email1 = user_email.Text;
+
+                ////string sql = @"UPDATE aspnet_Membership SET Email = @Email1 WHERE UserId = @Id";
+
+                ////SqlConnection con = new SqlConnection(cs);
+                ////SqlCommand cmd = new SqlCommand(sql, con);
+                ////cmd.Parameters.AddWithValue("@Email1", email1);
+                ////cmd.Parameters.AddWithValue("@Id", id);
+
+                ////con.Open();
+                ////cmd.ExecuteNonQuery();
+                ////con.Close();
+                
+                //MembershipUser user1 = Membership.GetUser(User.Identity.Name);
+                //user1.Email = email1;
+                //Membership.UpdateUser(user1);
+
+                ////Response.Redirect("ManageAccount.aspx");
+            }
         }
     }
 }
--- ManageAccount.aspx.designer.cs
+++ ManageAccount.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace WebApplication
         protected global::System.Web.UI.WebControls.Label lblId;
 
         /// <summary>
+        /// newR control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label newR;
+
+        /// <summary>
         /// user_name control.
         /// </summary>
         /// <remarks>
@@ -40,6 +49,78 @@ namespace WebApplication
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label user_email;
+
+        /// <summary>
+        /// user_realname control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox user_realname;
+
+        /// <summary>
+        /// editRealname control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button editRealname;
+
+        /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
+        /// RegularExpressionValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+
+        /// <summary>
+        /// user_address control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox user_address;
+
+        /// <summary>
+        /// editAddress control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button editAddress;
+
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+
+        /// <summary>
+        /// RegularExpressionValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
 
         /// <summary>
         /// ChangePassword1 control.
