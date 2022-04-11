066c98d327b0ad4a62dda6f224ced0220468c611
Jeriel Ng <ngjeriel59@gmail.com>
2022-04-12 04:28:46 +0800

    done error

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -11,7 +11,27 @@ namespace WebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //HttpCookie cookie = new HttpCookie("cookieName");
+            //cookie = Request.Cookies["Gender"];
 
+            //UserControl ucFemale = this.LoadControl("female.ascx") as UserControl;
+            //UserControl ucMale = this.LoadControl("male.ascx") as UserControl;
+            //UserControl general = this.LoadControl("general.ascx") as UserControl;
+
+            //if(cookie!= null)
+            //{
+            //    if (cookie.Value.Equals("F")){
+            //        contentAd.controls.Add(ucFemale);
+            //    }
+            //    else if (cookie.Value.Equals("M"))
+            //    {
+            //        contentAd.Controls.Add(ucMale);
+            //    }
+            //    else
+            //    {
+            //        contentAd.Controls.Add(general);
+            //    }
+            //}
         }
 
         protected void cartBtn_Click(object sender, EventArgs e)
---
+++ error404.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="error404.aspx.cs" Inherits="WebApplication.error404" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color:ivory;">
+    <form id="form1" runat="server">
+        <div style="text-align:center;font-weight:bold;background-color:antiquewhite;">
+            <h1 style="margin-top:200px;">Error 404: Page Not Found</h1>
+            <h2>Why dont come to have a look at our homepage?</h2>
+            <asp:Button ID="Button1" runat="server" Text="Button" style="width:150px;height:50px;font-size:30px;font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;background-color:burlywood;" OnClick="Button1_Click"/></div>
+    </form>
+</body>
+</html>
